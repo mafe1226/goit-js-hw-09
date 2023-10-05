4ee6475a13ed75a924129eecb69871678d259a29
Maria Fernanda Diaz <mafediazca@gmail.com>
2023-10-05 10:27:07 -0500

    tarea 1 realizada

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
 
+const buttonStart = document.getElementById("startBtn")
+const buttonStop = document.getElementById("stopBtn")
+const backGround = document.querySelector("body")
+let timerId = null;
+
+function changeBackgroundColor() {
+    const color = getRandomHexColor();
+    backGround.style.backgroundColor = color;
+
+}
+
+buttonStart.addEventListener("click" , (e) =>{
+    timerId = setInterval(changeBackgroundColor, 1000);
+});
+
+buttonStop.addEventListener("click", () => {
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
